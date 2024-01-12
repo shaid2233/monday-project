78863c301bfb1f6ccb95db203495334d8cf9c9ef
shai <shaidaniel22@gmail.com>
2024-01-13 00:07:40 +0200

    Update workflow to run test5.py with correct path

--- map services/test6.py
+++ map services/test6.py
@@ -1,11 +1,13 @@
+
+import os
 import requests
 import urllib.parse
 import json
 
 # Your monday.com API key (replace with your actual key)
-api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjMwNTc5NjI0MCwiYWFpIjoxMSwidWlkIjo0ODAyMDIyMSwiaWFkIjoiMjAyNC0wMS0wMVQxNDozODowNC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTg1MTU3MzksInJnbiI6ImV1YzEifQ.Rb509K9gkxYaFh2IoZYRq8uQKYoNgOZekepaJcryAIQ'  # Be sure to replace this with your actual API key
+api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjMwMzQ1OTc5NywiYWFpIjoxMSwidWlkIjo0ODAwNjMzMiwiaWFkIjoiMjAyMy0xMi0xOFQxMjoyMDozMi44MzNaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTg1MTU3MzksInJnbiI6ImV1YzEifQ.NqcmiUk34GRgaLwLMeJZ2Pl1ceHyn5MBL3w2ywOiYR8'
 # The ID of the board containing the addresses
-board_id = 1258969323  # Make sure this is the correct board ID
+board_id = 1258969323
 
 # The monday.com API endpoint for querying boards
 monday_api_url = 'https://api.monday.com/v2'
@@ -32,32 +34,37 @@ query = '''
 }
 ''' % board_id
 
+# ... [previous code]
+
 try:
     # Send request to monday.com API
     response = requests.post(url=monday_api_url, json={'query': query}, headers=headers)
-    response.raise_for_status()  # Raises an exception if the request failed
+    response.raise_for_status()
 except requests.exceptions.HTTPError as err:
     print(err)
 else:
     # If the request was successful, process each item
     for item in response.json()['data']['boards'][0]['items']:
-        # Extract the deal name from the item's name
-        deal_name = item['name']
-        # Extract the address from the column titled 'Address'
-        address = next((col['text'] for col in item['column_values'] if col['title'] == 'Address'), None)
-        if address:
-            # URL-encode the address to create a Google Maps URL
-            encoded_address = urllib.parse.quote(address)
-            google_maps_url = f"https://www.google.com/maps/search/?api=1&query={encoded_address}"
+        # Use the item's ID directly if that's what you mean by item_id
+        item_id = item['id']
+
+        # Your logic to use item_id here
+        if item_id:
+            # Example: Create a Google Maps URL using the item_id
+            # Modify this logic as per your requirement
+            google_maps_url = f"https://www.google.com/maps/search/?api=1&query={item_id}"
+
+            # ... [code to update the link column in Monday.com]
+
 
             # The ID of the column where you want to place the Google Maps link
-            link_column_id = 'link'  # This should be the actual ID of your "Link" column
+            link_column_id = 'link'
             
             # GraphQL mutation to update the link column with the Google Maps URL
             # Note: The value for a Link column should be a JSON object with `url` and `text` keys
             value = json.dumps({
                 "url": google_maps_url,
-                "text": deal_name  # Use the deal name as the display text for the link
+                "text": "View on Google Maps"
             })
             update_query = '''
             mutation ($itemId: Int!, $columnId: String!, $value: JSON!) {
@@ -65,7 +72,7 @@ else:
                     id
                 }
             }
-            ''' % board_id
+            ''' % (board_id)
             
             # Send the update request to monday.com
             update_payload = {
@@ -80,6 +87,6 @@ else:
             try:
                 update_response.raise_for_status()  # Raises an exception if the update request failed
                 # Output the updated information to the console
-                print(f"Item ID: {item['id']}, Deal '{deal_name}' Google Maps URL updated.")
+                print(f"Item ID: {item['id']}, Google Maps URL updated.")
             except requests.exceptions.HTTPError as err:
                 print(f"Failed to update item ID {item['id']}:", err)
